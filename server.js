3ff48b53c6f74344bdbcf88ba752a38a7dfc5da5
Miliya Immatty Lazar <miliyaninu@gmail.com>
2023-06-01 11:36:23 +0930

    commit

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
 
+// const logger = require('./middlewares/logger')
+// const sessions = require('./middlewares/sessions')
+
+
+//const eventsController = require('./controllers/events_controller')
+// const usersController = require('./controllers/users_controller')
+// const sessionsController = require('./controllers/sessions_controller')
+
 const app = express()
 const port = 3000
 
