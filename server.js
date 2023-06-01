ac86ac6d3652fd9580238e86c2b023d7c9f23b47
Miliya Immatty Lazar <miliyaninu@gmail.com>
2023-06-01 12:48:29 +0930

    add event and delete event

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const logger = require('./middlewares/logger')
 const sessions = require('./middlewares/sessions')
 
-
+const eventsController = require('./controllers/events_controller')
 const usersController = require('./controllers/users_controller')
 const sessionsController = require('./controllers/sessions_controller')
 
@@ -20,5 +20,6 @@ app.use(express.json())
 
 app.use(sessions)
 
+app.use('/api/events', eventsController)
 app.use('/api/users', usersController)
 app.use('/api/sessions', sessionsController)
