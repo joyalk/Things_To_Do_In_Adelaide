b64f5ede92adc79aa46ed7d5edf72d9dce850fb7
JoyalK <joyal.nijo2002@gmail.com>
2023-06-01 11:18:27 +0930

    server changes

--- server.js
+++ server.js
@@ -1,27 +1,6 @@
 const express = require('express')
 
-// const logger = require('./middlewares/logger')
-// const sessions = require('./middlewares/sessions')
-
-
-// const eventsController = require('./controllers/events_controller')
-// const usersController = require('./controllers/users_controller')
-// const sessionsController = require('./controllers/sessions_controller')
-
 const app = express()
 const port = 3000
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`))
-
-
-// app.use(logger)
-
-// app.use(express.static('client'))
-
-// app.use(express.json())
-
-// app.use(sessions)
-
-// app.use('/api/events', eventsController)
-// app.use('/api/users', usersController)
-// app.use('/api/sessions', sessionsController)
