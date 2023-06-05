9d88b8a74e241e638b4af9505cd7003a2a59eacc
Vishaka <vishaka.vishsg@gmail.com>
2023-06-05 16:48:10 +0930

    changes to login and logout

--- client/js/components/login.js
+++ client/js/components/login.js
@@ -1,69 +1,76 @@
-function renderLogIn() {
-    document.querySelector('#page').innerHTML = `
-      <section class='log-in'>
-        <form action="" onSubmit="logIn(event)">
-          <h2>Login:</h2>
-          <fieldset>
-            <label for="">Email: </label>
-            <input type="text" name="email">
-          </fieldset>
-          <fieldset>
-            <label for="">Password: </label>
-            <input type="password" name="password">
-          </fieldset>
-          <button>Log in</button>
-        </form>
-      </section>
-    `
-  }
+function renderLogin() {
+  document.querySelector('#page').innerHTML = `
+    <section class='log-in'>
+      <form action="" onSubmit="logIn(event)">
+        <h2>Login:</h2>
+        <fieldset>
+          <label for="">Email: </label>
+          <input type="text" name="email">
+        </fieldset>
+        <fieldset>
+          <label for="">Password: </label>
+          <input type="password" name="password">
+        </fieldset>
+        <button>Log in</button>
+      </form>
+    </section>
+  `
+}
 
 function logIn(event) {
-    event.preventDefault()
-    const form = event.target
-    
-    const data = Object.fromEntries(new FormData(form))
-    fetch('/api/sessions', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    })
-    .then(res => res.json())
-    .then(res => {
-      if (res.error) {
-        renderLogin()
-        renderError(res.error)
-      } else {
-        state.loggedInUser = res
-        renderEventList()
-      }
-    })
-  }
-  
+  event.preventDefault()
+  const form = event.target
+  // converts form data into an object
+  const data = Object.fromEntries(new FormData(form))
+  fetch('/api/sessions', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(res => {
+    if (res.error) {
+      renderLogin()
+      renderError(res.error)
+    } else {
+      state.loggedInUser = res.email
+      renderNavBar()
+      renderTreasureList()
+      
+      renderWelcomeMessage(res.message)
+    }
+  })
+}
+
 function renderError(errorMessage) {
+document.querySelector('#page').innerHTML =
+  `<h2 style='color: red;'>${errorMessage}</h2>` +
+  document.querySelector('#page').innerHTML
+}
+function renderWelcomeMessage(welcomeMessage) {
   document.querySelector('#page').innerHTML =
-    `<h2 style='color: red;'>${errorMessage}</h2>` +
+    `<h2 style='color: red;'>${welcomeMessage}</h2>` +
     document.querySelector('#page').innerHTML
   }
 
-function renderLogout() {
-    document.querySelector('#page').innerHTML = `
-      <section class='log-out'>
-        <form action="" onSubmit="logOut(event)">
-          <button>Log out</button>
-        </form>
-      </section>
-    `
-  }
-
+function renderLogOut() {
+  document.querySelector('#page').innerHTML = `
+    <section class='log-out'>
+      <form action="" onSubmit="logOut(event)">
+        <button>Log out</button>
+      </form>
+    </section>
+  `
+}
 function logOut(event) {
-    fetch('/api/sessions', {
-      method: 'DELETE',
-   
-    })
-    .then(res => {
-      if (userId) {
-        state.loggedOutUser = res
-        renderTreasureList()
-      }
-    })
-  }
+  fetch('/api/sessions', {
+    method: 'DELETE',
+ 
+  })
+  .then(res => {
+    if (userId) {
+      state.loggedOutUser = res
+      renderTreasureList()
+    }
+  })
+}
